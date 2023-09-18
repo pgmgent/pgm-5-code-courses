ad8cf1fe6285fe4dc673d29d08fd8b53a136a2b2
jannes lambrecht <janneslambrecht@hotmail.com>
2023-09-18 21:11:26 +0200

    update

---
+++ typescript/why/demo-mistake.js
@@ -0,0 +1,5 @@
+function validate(person, date) {
+  if(typeof age !== 'number') {
+      throw new Error('age is not a number')
+  }
+}
--- typescript/why/demo.js
+++ typescript/why/demo.js
@@ -1,5 +1,8 @@
-function validate(person, date) {
-  if(typeof age !== 'number') {
-      throw new Error('age is not a number')
-  }
+function validateTs() {
+    var age = document.getElementById('age').value;
+    var name = document.getElementById('name').value;
+    var calculatedAge = age * 7;
+    if (typeof age !== 'number') {
+        throw new Error('age is not a number');
+    }
 }
